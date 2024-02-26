731862757222374c79791699f31219ffc60b7cad
PedroMiguel7 <pedromiguelmgaldino@gmail.com>
2024-02-26 10:18:22 -0300

    feat: initial fundamentals typescript

---
+++ fundamentals/app.ts
@@ -0,0 +1,7 @@
+const sum = (a: number, b: number): number => {
+  const result = a + b;
+  console.log(result);
+  return result;
+};
+
+sum(1, 2);
---
+++ fundamentals/index.js
@@ -0,0 +1,8 @@
+const sum = (a, b) => {
+  const result = a + b;
+  console.log(result);
+  return result;
+};
+
+sum(1, 2);
+sum("pedro", "miguel");
