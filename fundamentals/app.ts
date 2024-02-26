3d6639e7597ae1993bd78760bd9d6bd44f91a57a
PedroMiguel7 <pedromiguelmgaldino@gmail.com>
2024-02-26 10:29:35 -0300

    feat: variables and types

--- fundamentals/app.ts
+++ fundamentals/app.ts
@@ -1,7 +1,14 @@
-const sum = (a: number, b: number): number => {
-  const result = a + b;
-  console.log(result);
-  return result;
-};
+var a = "a";
+let b: string = "b";
+const c = "c";
 
-sum(1, 2);
+let n: number = 2;
+let x: boolean = true;
+let m: string | number = 2;
+
+// b = "2";
+// c = "3"; // Error because c is a constant
+
+// b = 2; // Error because b is a string
+// m = "pedro miguel";
+// m = false; // Error because m is a string or a number
--- fundamentals/index.js
+++
@@ -1,8 +0,0 @@
-const sum = (a, b) => {
-  const result = a + b;
-  console.log(result);
-  return result;
-};
-
-sum(1, 2);
-sum("pedro", "miguel");
