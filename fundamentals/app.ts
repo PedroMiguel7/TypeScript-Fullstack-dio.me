80b851956c87ecba6ae664fac6487023c1d5a6c0
PedroMiguel7 <pedromiguelmgaldino@gmail.com>
2024-02-27 11:27:32 -0300

    feat: manipulating arrays

--- fundamentals/app.ts
+++ fundamentals/app.ts
@@ -1,5 +1,10 @@
 const arrayNum: Array<number> = [1, 2, 3, 4];
 
-console.log(arrayNum);
-arrayNum.pop();
-console.log(arrayNum);
+arrayNum.forEach((num) => {
+  console.log(num);
+});
+
+arrayNum.map((num) => {
+  console.log(num);
+  return num;
+});
