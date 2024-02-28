3457ca9512ba84726c6b8693c5d4a56e28e4d920
PedroMiguel7 <pedromiguelmgaldino@gmail.com>
2024-02-28 15:01:18 -0300

    Feat: Visibility of attributes and methods

--- fundamentals/app.ts
+++ fundamentals/app.ts
@@ -1,12 +1,5 @@
-import { CompanyAccount } from "./class/CompanyAccount";
 import { PeopleAccount } from "./class/PeopleAccount";
 
 const peopleAccount: PeopleAccount = new PeopleAccount(123, "John Doe", 123456);
-peopleAccount.deposit(100);
 
-const companyAccount: CompanyAccount = new CompanyAccount(
-  123,
-  "Company Inc",
-  654321
-);
-companyAccount.deposit(100);
+peopleAccount.deposit(100);
--- fundamentals/class/DioAccount.ts
+++ fundamentals/class/DioAccount.ts
@@ -1,15 +1,35 @@
+export enum AccountStatus {
+  ACTIVE = "active",
+  INACTIVE = "inactive",
+  BLOCKED = "blocked",
+  CANCELED = "canceled",
+}
+
 export abstract class DioAccount {
-  name!: string;
+  private name!: string;
   accountNumber!: number;
   balance: number = 0;
+  private status: AccountStatus = AccountStatus.ACTIVE;
 
   constructor(name: string, accountNumber: number) {
     this.name = name;
     this.accountNumber = accountNumber;
   }
 
+  setName(name: string) {
+    this.name = name;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
   deposit(amount: number) {
-    console.log(`Deposited ${amount} in account ${this.accountNumber}`);
+    if (this.validateStatus()) {
+      console.log(`Deposited ${amount} in account ${this.accountNumber}`);
+      return;
+    }
+    throw new Error("Account is not active");
   }
 
   withdraw(amount: number) {
@@ -18,5 +38,9 @@ export abstract class DioAccount {
 
   getBalance() {
     console.log(this.balance);
+  }
+
+  validateStatus(): boolean {
+    return this.status === AccountStatus.ACTIVE;
   }
 }
