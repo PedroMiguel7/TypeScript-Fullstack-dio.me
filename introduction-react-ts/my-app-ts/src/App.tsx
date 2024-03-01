48191caab99347427c00fb6fedda095dce4a48c3
PedroMiguel7 <pedromiguelmgaldino@gmail.com>
2024-03-01 09:20:39 -0300

    feat: useState

--- introduction-react-ts/my-app-ts/src/App.tsx
+++ introduction-react-ts/my-app-ts/src/App.tsx
@@ -1,13 +1,21 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Button } from "@chakra-ui/react";
 
-import { Card } from "./components/card";
+// import { Card } from "./components/card";
 import { Layout } from "./components/layout";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
     <ChakraProvider>
       <Layout>
-        <Card />
+        {/* <Card /> */}
+        <div>
+          <p>{value}</p>
+          <Button onClick={() => setValue(value - 1)}>Decrement</Button>
+          <Button onClick={() => setValue(value + 1)}>Increment</Button>
+        </div>
       </Layout>
     </ChakraProvider>
   );
