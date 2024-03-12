8fd49104d63ef6de7d0ffc7b389b2b8eed96f7ed
PedroMiguel7 <pedromiguelmgaldino@gmail.com>
2024-03-12 15:03:08 -0300

    feat: create routers

--- node-ts/src/index.ts
+++ node-ts/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello World");
+import express, { Request, Response } from "express";
+
+const server = express();
+
+server.use(express.json());
+
+const port = 5000;
+
+server.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({ message: "Hello World" });
+});
+
+server.post("/user", (req: Request, res: Response) => {
+  const body = req.body;
+  console.log(body);
+  return res.status(201).json({ message: "User created" });
+});
+
+server.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
