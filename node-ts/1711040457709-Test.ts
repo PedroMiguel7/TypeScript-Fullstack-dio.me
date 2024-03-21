38bd04225a77bae95c35d4390ed75a7031b44d2c
PedroMiguel7 <pedromiguelmgaldino@gmail.com>
2024-03-21 14:07:40 -0300

    feat: add migration and ormconfig files

---
+++ node-ts/1711040457709-Test.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Test1711040457709 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
