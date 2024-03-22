6f22b22b9fb230f194c7075413a951c59633ecc2
PedroMiguel7 <pedromiguelmgaldino@gmail.com>
2024-03-22 13:22:38 -0300

    feat: export connection

--- node-ts/src/database/index.ts
+++ node-ts/src/database/index.ts
@@ -3,5 +3,13 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "./src/database/db.sqlite",
-  migrations: ["./src/database/migrations"],
+  migrations: ["./src/database/migrations/*.ts"],
 });
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database is running");
+  })
+  .catch((error) => {
+    console.log("Error on database connection: " + error);
+  });
--- node-ts/src/index.ts
+++ node-ts/src/index.ts
@@ -2,18 +2,9 @@ import "reflect-metadata";
 
 import express, { Request, Response } from "express";
 
-import { AppDataSource } from "./database";
 import { router } from "./routes";
 
 const server = express();
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Database is running");
-  })
-  .catch((error) => {
-    console.log("Error on database connection: " + error);
-  });
 
 server.use(express.json());
 server.use(router);
