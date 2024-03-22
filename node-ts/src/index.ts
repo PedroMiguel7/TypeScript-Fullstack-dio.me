9218baded8c0171067d4a6e211492d7496a3be7d
PedroMiguel7 <pedromiguelmgaldino@gmail.com>
2024-03-22 13:12:49 -0300

    feat: implements dataSource

--- node-ts/src/index.ts
+++ node-ts/src/index.ts
@@ -1,8 +1,19 @@
+import "reflect-metadata";
+
 import express, { Request, Response } from "express";
 
+import { AppDataSource } from "./database";
 import { router } from "./routes";
 
 const server = express();
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database is running");
+  })
+  .catch((error) => {
+    console.log("Error on database connection: " + error);
+  });
 
 server.use(express.json());
 server.use(router);
